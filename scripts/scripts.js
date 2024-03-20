76747c0ff88d59860c0e4d602242e29fad1c5512
matheus kamers <mkamersandrade@gmail.com>
2024-03-19 22:56:58 -0300

    EX 4

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -15,7 +15,7 @@ signinBtn.addEventListener("click", function() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    if (!username || !password) {
+    if (!username && !password) {
         alert("Por favor, preencha todos os campos.");
     } else {
         let found = false;
@@ -31,3 +31,28 @@ signinBtn.addEventListener("click", function() {
     }
     }
 });
+
+function register() {
+    const username = document.getElementById("username-register").value;
+    const password = document.getElementById("password-register").value;
+
+    if (!username || !password) {
+        alert("Por favor, preencha todos os campos.");
+    } else {
+        let exist = false;
+        for (const user of dados) {
+            if (user.username === username) {
+                exist = true;
+                alert("Usuário já cadastrado.");
+                break;
+            }
+        }
+        if (!exist) {
+            dados.push({
+                username: username,
+                password: password
+            });
+            alert("Usuário cadastrado com sucesso!");
+        }
+    }
+}
