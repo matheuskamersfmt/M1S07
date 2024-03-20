3139f6a59fe4a99247e0a5913c21d74a38556d47
matheus kamers <mkamersandrade@gmail.com>
2024-03-19 22:52:38 -0300

    EX 3

---
+++ scripts/scripts.js
@@ -0,0 +1,33 @@
+const dados = [
+    {
+        username: 'mkamersandrade@gmail.com',
+        password: 'matheus123'
+    },
+    {
+        username: 'adm@gmail.com',
+        password: 'adm123'
+    },
+]
+const signinBtn = document.getElementById("signin");
+const signupBtn = document.getElementById("signup");
+
+signinBtn.addEventListener("click", function() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (!username || !password) {
+        alert("Por favor, preencha todos os campos.");
+    } else {
+        let found = false;
+    for (const user of dados) {
+        if (user.username === username && user.password === password) {
+            found = true;
+            alert("Login bem-sucedido!");
+            break;
+        }
+    }
+    if (!found) {
+        alert("Nome de usuário ou senha incorretos.");
+    }
+    }
+});
